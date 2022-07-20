b3701ba302c2639ecb35988172a33649030bc160
David Borja <DBorja95@SD-0401.local>
2022-07-20 12:55:26 -0600

    css and html done

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const form = document.getElementById("form");
+const password1El = document.getElementById("password1El");
+const password2El = document.getElementById("password2El");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");
 
+function processFormData(e) {
+  console.log(e);
+  e.preventDefault();
+}
+
+// Event Listener
+form.addEventListener("submit", processFormData);
